5aa762c7e7a64efa83fdbfea73ad6a2448561e86
Nikimaru <107780724+Nigimaru@users.noreply.github.com>
2025-12-11 17:57:23 +0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+
+const inter = Inter({ subsets: ['latin'] });
+
+export const metadata: Metadata = {
+  title: 'Polymeow',
+  description: 'Guess if the cat is Silver or Gold!',
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
